fdf4171d9da15e4ae70cb70b1847145bdbbc8484
CatalinaSandu <sanducatalina10@yahoo.com>
2016-08-08 12:07:22 +0300

    added orders status

--- app/controllers/api/v1/orders.rb
+++ app/controllers/api/v1/orders.rb
@@ -4,6 +4,40 @@ module API
       include API::V1::Defaults
 
       resource :orders do
+
+
+        desc "checking code"
+        params do
+          requires :order_code, type: String
+        end
+
+        get :check_code do
+          order_code = params[:order_code].split('-')
+          order_id = order_code.first
+          menu_id = order_code.last
+
+            begin
+                order = Order.where(id: order_id).first
+            rescue
+                order = nil
+            end
+            begin
+                menu = Menu.where(id: menu_id).first
+            rescue
+                menu = nil
+            end
+            if order_code.blank?
+              {error_code: 401, error_message:"No order code."}
+            elsif menu_id.blank?
+              {error_code: 401, error_message:"No menu id."}
+            elsif order_id.blank?
+              {error_code: 401, error_message:"No order id"}
+            else
+               { order_status: order.order_status }
+            end
+        end
+
+
         desc "Return all orders"
         get "", root: :orders do
           Order.all
@@ -52,9 +86,10 @@ module API
               user_id: user.id,
               order_status: "Pending"
             })
+            { status: 201, message: "order created" }
           end
-        end      
-      
+
+        end   
 
 
       end
